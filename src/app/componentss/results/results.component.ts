f513c312034f512205fa4feb3bcd6cedd41bb1b4
maralm <hm6255909@gmail.com>
2023-05-30 23:44:07 +0300

    diffetler cözüldü ve  cevapların kayıt edildiği bir table oluşturuldu

--- src/app/componentss/results/results.component.ts
+++ src/app/componentss/results/results.component.ts
@@ -4,12 +4,13 @@ import {delay, map, Observable, tap} from "rxjs";
 import {WhereAnswersRecordedService} from "../service/where-answers-recorded.service";
 import {MatTable} from "@angular/material/table";
 import {ApiService} from "../service/api.service";
-export interface PeriodicElement {
-  name: string;
-  position: string;
-  weight: string;
-  symbol: string;
-}
+import {dateTimestampProvider} from "rxjs/internal/scheduler/dateTimestampProvider";
+// export interface PeriodicElement {
+//   name: string;
+//   position: string;
+//   weight: string;
+//   symbol: string;
+// }
 
 export interface result {
   soru: string;
@@ -24,18 +25,18 @@ export interface result {
 }
 
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: "1", name: 'Hydrogen', weight: "1.0079", symbol: 'H'},
-  {position: "2", name: 'Helium', weight: "4.0026", symbol: 'He'},
-  {position: "3", name: 'Lithium', weight:" 6.941", symbol: 'Li'},
-  {position: "4", name: 'Beryllium', weight: "9.0122", symbol: 'Be'},
-  {position: "5", name: 'Boron', weight: "10.811", symbol: 'B'},
-  {position:" 6", name: 'Carbon', weight: "12.0107", symbol: 'C'},
-  {position: "7", name: 'Nitrogen', weight: "14.0067", symbol: 'N'},
-  {position: "8", name: 'Oxygen', weight: "15.9994", symbol: 'O'},
-  {position: "9", name: 'Fluorine',weight: "18.9984", symbol: 'F'},
-  {position: "10", name: 'Neon', weight: "20.1797", symbol: 'Ne'},
-];
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {position: "1", name: 'Hydrogen', weight: "1.0079", symbol: 'H'},
+//   {position: "2", name: 'Helium', weight: "4.0026", symbol: 'He'},
+//   {position: "3", name: 'Lithium', weight:" 6.941", symbol: 'Li'},
+//   {position: "4", name: 'Beryllium', weight: "9.0122", symbol: 'Be'},
+//   {position: "5", name: 'Boron', weight: "10.811", symbol: 'B'},
+//   {position:" 6", name: 'Carbon', weight: "12.0107", symbol: 'C'},
+//   {position: "7", name: 'Nitrogen', weight: "14.0067", symbol: 'N'},
+//   {position: "8", name: 'Oxygen', weight: "15.9994", symbol: 'O'},
+//   {position: "9", name: 'Fluorine',weight: "18.9984", symbol: 'F'},
+//   {position: "10", name: 'Neon', weight: "20.1797", symbol: 'Ne'},
+// ];
 
 
 @Component({
@@ -49,37 +50,41 @@ export class ResultsComponent {
   soyisim :string = "maral"
   numara :string="1";
   isimSoyisimNumara:string= this.isim+this.soyisim+this.numara;
-  resultTable:result[]=[
+  resultTable:any[]=[
     {soru:"1",isim: "hamza",soyisim:"maral",numara:"1",
-      dogruCevap:"true",benimCevabim:"benimCevabım",category:"category",difficulty:"difficulty",type:"type"},
+      dogruCevap:"true",benimCevabim:"benimCevabım",category:"category",
+      difficulty:"difficulty",type:"type"},
   ];
+  resultTablelist:any[]= []
   displayedColumnsresult: string[] = ['soru', 'isim',
     'soyisim', 'numara','dogruCevap', 'benimCevabim',"category",
     "difficulty","type" ];
    url = "https://ng-shopapp-3666b-default-rtdb.firebaseio.com/answers/" + this.isimSoyisimNumara;
-   url2 = "https://ng-shopapp-3666b-default-rtdb.firebaseio.com/products";
-   ELEMENT_DATA2: PeriodicElement[]  = [
-     {position: "1", name: 'Hydrogen', weight: "1.0079", symbol: 'H'},
-     {position: "2", name: 'Helium', weight: "4.0026", symbol: 'He'},
-     {position: "3", name: 'Lithium', weight:" 6.941", symbol: 'Li'},
-     {position: "4", name: 'Beryllium', weight: "9.0122", symbol: 'Be'},
-     {position: "5", name: 'Boron', weight: "10.811", symbol: 'B'},
-     {position:" 6", name: 'Carbon', weight: "12.0107", symbol: 'C'},
-     {position: "7", name: 'Nitrogen', weight: "14.0067", symbol: 'N'},
-     {position: "8", name: 'Oxygen', weight: "15.9994", symbol: 'O'},
-     {position: "9", name: 'Fluorine',weight: "18.9984", symbol: 'F'},
-     {position: "10", name: 'Neon', weight: "20.1797", symbol: 'Ne'},
-  ];
+  //  url2 = "https://ng-shopapp-3666b-default-rtdb.firebaseio.com/products";
+  //  ELEMENT_DATA2: PeriodicElement[]  = [
+  //    {position: "1", name: 'Hydrogen', weight: "1.0079", symbol: 'H'},
+  //    {position: "2", name: 'Helium', weight: "4.0026", symbol: 'He'},
+  //    {position: "3", name: 'Lithium', weight:" 6.941", symbol: 'Li'},
+  //    {position: "4", name: 'Beryllium', weight: "9.0122", symbol: 'Be'},
+  //    {position: "5", name: 'Boron', weight: "10.811", symbol: 'B'},
+  //    {position:" 6", name: 'Carbon', weight: "12.0107", symbol: 'C'},
+  //    {position: "7", name: 'Nitrogen', weight: "14.0067", symbol: 'N'},
+  //    {position: "8", name: 'Oxygen', weight: "15.9994", symbol: 'O'},
+  //    {position: "9", name: 'Fluorine',weight: "18.9984", symbol: 'F'},
+  //    {position: "10", name: 'Neon', weight: "20.1797", symbol: 'Ne'},
+  // ];
 
-  ELEMENT_DATA3: PeriodicElement[]  = [
-    {position: "1", name: 'Hydrogen', weight: "1.0079", symbol: 'H'}
-  ];
+  // ELEMENT_DATA3: PeriodicElement[]  = [
+  //   {position: "1", name: 'Hydrogen', weight: "1.0079", symbol: 'H'}
+  // ];
   products1: any[] = [];
   products1key: any[] = [];
   products2: any[] = [];
   products2key: any[] = [];
-   b: any;
-   anahtar: any;
+  b : any[];
+  resultlist: any[];
+  anahtar:string;
+  anahtarlist: any[];
 
 
   constructor(private http: HttpClient,protected mn:WhereAnswersRecordedService,private changeDetectorRef: ChangeDetectorRef,private setapi:ApiService) {
@@ -101,8 +106,7 @@ export class ResultsComponent {
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 
-  mlk= this.mn.m
-  dataSource= this.ELEMENT_DATA3;
+  // dataSource= this.ELEMENT_DATA3;
 
   getresult(){
     this.isimSoyisimNumara= this.isim+this.soyisim+this.numara;
@@ -114,15 +118,25 @@ export class ResultsComponent {
           const products:any[] = [];
           // const productsnew: any[] = [];
           products.push(data)
-          this.products1.push(data)
+          this.products2.push(data)
           // console.log(data)
+          this.products1.push(this.products2)
+          console.log(data)
+
 
           for(const key in data) {
+
+            this.products2key.push(key)
+            // this.products2key.push(this.anahtar)
             // productsnew.push({ ...data, id: key });
-            this.products1key.push(key)
+            this.products1key.push(this.products2key)
+            console.log("iyii akşaömlar")
+            console.log(this.products1key[0])
 
 
           }
+          this.products2 = [];
+          this.products2key = [];
 
 
 
@@ -135,32 +149,47 @@ export class ResultsComponent {
 
   resultss(){
     this.isimSoyisimNumara= this.isim+this.soyisim+this.numara;
-    this.url = "https://ng-shopapp-3666b-default-rtdb.firebaseio.com/answers/" + this.isimSoyisimNumara;
+    this.url = "https://ng-shopapp-3666b-default-rtdb.firebaseio.com/answers/" + this.isimSoyisimNumara ;
     // let b:any[]
     this.getresult().subscribe(data => {
 
-      for (let i = 0; i < this.products1key.length; i++) {
-        this.anahtar= this.products1key[i]
-        this.b=this.products1[0][this.anahtar]
-        console.log(this.b)
-        console.log(this.anahtar)
-        console.log(this.b["Soru"])
-        console.log(this.b["dogruCevap"])
-        console.log(this.b["benimCevabim"])
-        console.log(this.b["category"])
-        console.log(this.b["type"])
-        console.log(this.b["difficulty"])
+      for (let i = 0; i < this.products1key[0].length; i++) {
+        this.anahtarlist= this.products1key[0]
+        // this.anahtar=this.products1[0][this.resultlist]
+        // this.resultlist.push(this.b)
+        // this.anahtarlist.push(this.anahtar)
+        // console.log(this.resultlist[])
+        // console.log(this.resultlist[])
+        // console.log(this.anahtar)
 
-        // console.log()
-        // b[i].Soru
-        // // b[i].isim
-        // // b[i].soyisim
-        // // b[i].numara
-        // b[i].dogruCevap
-        // b[i].benimCevabım
-        // b[i].category
-        // b[i].difficulty
-        // b[i].type
+
+        // console.log(this.anahtarlist[i])
+        console.log(this.products1[0][0][this.anahtarlist[i]])
+        console.log("baykemal")
+        let deneme = this.products1[0][0][this.anahtarlist[i]]
+        // console.log(this.b["Soru"])
+        // console.log(deneme["benimCevabim"])
+        // console.log(this.b["benimCevabim"])
+        // console.log(this.b["category"])
+        // console.log(this.b["type"])
+        // console.log(this.b["difficulty"])
+///////////////////////////////////////
+        let resulrdemeyanilma =
+            {soru:deneme["Soru"],isim:this.isim,soyisim:this.soyisim,numara:this.numara,
+              dogruCevap:deneme["dogruCevap"],benimCevabim:deneme["benimCevabim"],
+              category:deneme["category"],
+              difficulty:deneme["difficulty"],type:deneme["type"]}
+
+        this.resultTablelist.push(resulrdemeyanilma)
+        // this.resultTablelist.push(c)
+        console.log(this.resultTablelist[i])
+        this.resultTable.push(this.resultTablelist[i])
+
+        // console.log(this.resultTable[0])
+
+        this.changeDetectorRef.detectChanges()
+        this.table.renderRows() // Tabloyu yeniden render et
+        //////////////////////////////////////////////
         // let resulrdemeyanılma =[
         //   {soru:b[i].soru,isim:this.isim,soyisim:this.soyisim,numara:this.numara,
         //     dogruCevap:b[i].dogruCevap,benimCevabim:b[i].benimCevabim,
@@ -168,48 +197,29 @@ export class ResultsComponent {
         // ]
         // console.log(resulrdemeyanılma)
 
-
-
-        this.dataSource=this.ELEMENT_DATA2
+        // this.dataSource=this.ELEMENT_DATA2
         // this.resultTable=
 
-        this.ELEMENT_DATA2.push({position:i.toString(),name:this.anahtar,weight:this.products1[0][this.anahtar].soru,symbol:this.products1[0][this.anahtar]["benim cevabım"]})
-        this.dataSource=this.ELEMENT_DATA2
-        this.changeDetectorRef.detectChanges()
-        this.table.renderRows(); // Tabloyu yeniden render et
+        // this.ELEMENT_DATA2.push({position:i.toString(),name:this.anahtar,weight:this.products1[0][this.anahtar].soru,symbol:this.products1[0][this.anahtar]["benim cevabım"]})
+        // this.dataSource=this.ELEMENT_DATA2
+
 
       }
-      console.log(this.b)
-      console.log(this.anahtar)
+      //for sonu
+      this.resultTablelist.splice(0, this.resultTablelist.length);
+      this.resultTable.splice(0, this.resultTable.length);
+      this.products1key.splice(0,  this.products1key.length);
+      this.anahtarlist.splice(0,  this.anahtarlist.length);
 
-      // }
-      ////////////////////// let anahtar = "-NWbE8j-UbLDF8Hn7eor"; // Görüntülemek istediğiniz anahtar
-     ////////////////////////// console.log(this.products1[0][anahtar])
-      //////////////////////// console.log(this.products1key)
+      // console.log(this.b)
+      // console.log(this.anahtar)
+
     });
     // let anahtar = "-NWbE8j-UbLDF8Hn7eor"; // Görüntülemek istediğiniz anahtar
     // for (const key in this.products1key) {
     //   console.log(key)
     // }
 
-    // console.log(this.products1[0][anahtar])
-    // console.log(this.products1key)
-
-
-
-    // const obj: { [key: string]: { [key: string]: string | number } } = {
-    //   "-NWbE8ddz6Ef99MLMbMt": {
-    //     "benim cevabım": "Scrappy",
-    //     "doğru cevap": "Scoobert",
-    //     "soru": 1
-    //   },
-    //   "-NWbE8j-UbLDF8Hn7eor": {
-    //     "benim cevabım": "Hamm",
-    //     "doğru cevap": "Mr. Potato Head",
-    //     "soru": 2
-    //   },
-    //   // Diğer öğeler...
-    // };
     //
     // for (const key in obj) {
     //   const item = obj[key];
